d7f57d22bcaadf64027aee8d0e0e0fda3fa65262
Amy Ruan <nyc.amy@gmail.com>
2014-02-18 17:44:24 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenTable::Application.config.secret_key_base = '5f79eb0d3812aa72b79ae367c13827f0735cca6d0d903a53090d5f15d922e9039d37e746e66e7b850a1462a371f4b660afd47ef56fb9ce489d0013f936998326'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenTable::Application.config.session_store :cookie_store, key: '_open_table_session'
